aab52ba8ebba4939a5f14ceb7bcafe8a7c4f1bae
StahlFerro <StahlFerro@users.noreply.github.com>
2019-05-01 02:19:48 +0700

    Testing jquery for candidate of menu-bar content loading

--- jsbrain/menu_handler.js
+++ jsbrain/menu_handler.js
@@ -4,9 +4,16 @@ window.$ = window.jQuery = require('jquery');
 let create_button = document.querySelector('#create_button')
 let split_button = document.querySelector('#split_button');
 let exit_button = document.querySelector('#exit_button');
+let display_panel = document.getElementById('display_panel');
 
 $(create_button).click(function() {
-    console.log('yes');
+    console.log('create button pressed');
+    $(display_panel).html("<p>Lmao</p>");
+});
+
+$(split_button).click(function () {
+    console.log('split button pressed');
+    $(display_panel).html("<h1 class='title is-1'>follow me on microsoft excel</h1>");
 });
 
 exit_button.addEventListener('click', () => {
