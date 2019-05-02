1f9b94364bb5513a52203ce40fa0ced6f09181af
StahlFerro <StahlFerro@users.noreply.github.com>
2019-05-02 15:10:11 +0700

    Minimize button now functioning

--- jsbrain/menu_handler.js
+++ jsbrain/menu_handler.js
@@ -1,6 +1,13 @@
 const remote = require("electron").remote;
 window.$ = window.jQuery = require('jquery');
 
+let create_menu = document.getElementById('create_menu');
+let split_menu = document.getElementById('split_menu');
+let create_panel = document.getElementById('create_panel');
+let split_panel = document.getElementById('split_panel');
+
+let minimize_button = document.getElementById('minimize_button');
+let exit_button = document.getElementById('exit_button');
 let display_panel = document.getElementById('display_panel');
 
 // window.addEventListener("load", show_create_panel);
@@ -22,7 +29,13 @@ function show_split_panel() {
 
 $(create_menu).click(show_create_panel);
 $(split_menu).click(show_split_panel);
-exit_button.addEventListener('click', () => {
+
+minimize_button.addEventListener("click", () => {
+    var window = remote.getCurrentWindow();
+    window.minimize();
+})
+
+exit_button.addEventListener("click", () => {
     var window = remote.getCurrentWindow();
     window.close();
 });
