39a228861eedecc5a7ef993d424327d75c0641b4
StahlFerro <StahlFerro@users.noreply.github.com>
2019-05-10 11:46:35 +0700

    bulmamods.scss cleanup, quintcell generator function for the 5-row sequence table

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 The cross-platform desktop swiss-army knife for GIFs and APNGs. Create, split, convert or alter GIFs and animated PNGs with easy-to-use-but-powerful-when-needed controls.
 
 ```
-⚠️ WARNING ⚠ ️
-The app is currently under rigorous daily development and is incomplete. Clone it at your own risk. All feedbacks appreciated
+⚠️ WARNING ⚠
+The app is currently under rigorous daily development and is incomplete. Clone it at your own risk. All feedbacks are appreciated
 ```
 
 TridentFrame consists of two main parts. The python backend handling image processing, and the web frontend (HTML-CSS-JS) for the UI, image previews and input controls.
 
 ### Built with
 *   [Electron](https://electronjs.org/) : Main framework
-*   [Pillow](https://python-pillow.org/) : Image parsing library
+*   [Pillow](https://python-pillow.org/) : GIF parsing
 *   [pyAPNG](https://github.com/eight04/pyAPNG) : APNG parsing
 *   [jQuery](http://jquery.com/) : Menu handling
 *   [zeroRPC](https://www.zerorpc.io/) : Connecting the python backend to the electron frontend
--- jsbrain/createfragment.js
+++ jsbrain/createfragment.js
@@ -5,6 +5,7 @@ window.$ = window.jQuery = require('jquery');
 
 // let sequence_carousel = document.getElementById('sequence_carousel')
 let sequences = null;
+var sequence_body = document.getElementById('sequence_body')
 let load_imgs_button = document.getElementById('load_imgs_button')
 let clear_imgs_button = document.getElementById('clear_imgs_button')
 
@@ -23,7 +24,8 @@ load_imgs_button.addEventListener("click", () => {
             console.error(error);
         } else {
             sequences = res.sequences;
-            console.log("obtained sequences", sequences)
+            console.log("obtained sequences", sequences);
+            quintcell_generator(sequences);
             // carousel_tags = ''
             // for (const [index, ipath] of img_paths.entries()) {
             //     console.log(ipath)
@@ -43,7 +45,21 @@ clear_imgs_button.addEventListener('click', () => {
 });
 
 function quintcell_generator(paths) {
-
+    sequence_body.innerHTML = '';
+    for (var row = 0; row < Math.ceil(paths.length / 5); row++) {
+        var tr = document.createElement('TR');
+        for (var c = 0; c < 5; c++) {
+            const index = (row * 5) + c;
+            const data = paths[index];
+            if (data === undefined) {continue;}
+            var td = document.createElement('TD');
+            var img = document.createElement('IMG');
+            img.src = data;
+            td.appendChild(img);
+            tr.appendChild(td);
+        }
+        sequence_body.appendChild(tr);
+    }
 }
 
 // (function () {
