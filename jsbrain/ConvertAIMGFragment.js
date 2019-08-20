38ea2ba6e0452c11141f2335e96b0a1070598fef
StahlFerro <StahlFerro@users.noreply.github.com>
2019-08-20 13:28:33 +0700

    Center-positioned Convert AIMG stages, added duration and fps to Convert image information

--- jsbrain/ConvertAIMGFragment.js
+++ jsbrain/ConvertAIMGFragment.js
@@ -10,12 +10,15 @@ let CON_load_aimg_button = document.getElementById('CON_load_aimg_button');
 let CON_clear_aimg_button = document.getElementById('CON_clear_aimg_button');
 let CON_bgprev_button = document.getElementById('CON_bgprev_button');
 let CON_in_cell = document.getElementById('CON_in_cell');
+let CON_in_stage = document.getElementById('CON_in_stage');
+let CON_in_path = document.getElementById('CON_in_path');
 
 let CON_in_name = document.getElementById('CON_in_name');
 let CON_in_format = document.getElementById('CON_in_format');
 let CON_in_dimensions = document.getElementById('CON_in_dimensions');
-let CON_in_stage = document.getElementById('CON_in_stage');
-let CON_in_path = document.getElementById('CON_in_path');
+let CON_in_duration = document.getElementById('CON_in_duration');
+let CON_in_fps = document.getElementById('CON_in_fps');
+
 
 let convert_checkerbg_active = false;
 
@@ -59,6 +62,8 @@ function conLoadAIMG(res) {
     CON_in_name.innerHTML = escapeHtml(res.name);
     CON_in_format.innerHTML = res.extension;
     CON_in_dimensions.innerHTML = `${res.width} x ${res.height}`;
+    CON_in_duration.innerHTML = `${res.avg_duration} seconds`;
+    CON_in_fps.innerHTML = `${res.fps} fps`;
     CON_in_stage.src = res.absolute_url;
     CON_in_path.value = res.absolute_url;
 }
@@ -69,6 +74,8 @@ function conClearAIMG() {
     CON_in_name.innerHTML = '-';
     CON_in_format.innerHTML = '-';
     CON_in_dimensions.innerHTML = '-';
+    CON_in_duration.innerHTML = '-';
+    CON_in_fps.innerHTML = '-';
     CON_in_stage.src = '';
     CON_in_path.value = '';
     session.clearCache(() => {});
