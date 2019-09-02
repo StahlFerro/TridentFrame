20846212fb2c24e8fa79f768e85e2083fe5db054
StahlFerro <StahlFerro@users.noreply.github.com>
2019-09-03 03:47:49 +0700

    Menuhandler functionality now working using Vue

--- App.vue
+++ App.vue
@@ -30,34 +30,48 @@
                     <div class="left-menu">
                         <aside class="menu has-text-centered" style="margin: 0;">
                             <ul class="menu-list left-menu">
-                                <li id="create_box" class="menu-item is-selected"><a id="create_menu">
-                                    <span class="icon is-large"><i class="fas fa-image fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">Create AIMG</p>
-                                </a></li>
-                                <li id="split_box" class="menu-item"><a id="split_menu">
-                                    <span class="icon is-large"><i class="far fa-images fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">Split AIMG</p>
-                                </a></li>
-                                <li id="modify_box" class="menu-item"><a id="modify_menu">
-                                    <span class="icon is-large"><i class="fas fa-exchange-alt fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">Modify AIMG</p>    
-                                </a></li>
-                                <li id="buildsprite_box" class="menu-item"><a id="buildsprite_menu">
-                                    <span class="icon is-large"><i class="fas fa-border-all fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">Build Spritesheet</p>    
-                                </a></li>
-                                <li id="slicesprite_box" class="menu-item"><a id="slicesprite_menu">
-                                    <span class="icon is-large"><i class="fas fa-th fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">Slice Spritesheet</p>    
-                                </a></li>
-                                <li id="settings_box" class="menu-item"><a id="settings_menu">
-                                    <span class="icon is-large"><i class="fas fa-cog fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">Settings</p>
-                                </a></li>
-                                <li id="about_box" class="menu-item"><a id="about_menu">
-                                    <span class="icon is-large"><i class="fas fa-info-circle fa-2x fa-inverse"></i></span>
-                                    <p class="is-white-d">About</p>
-                                </a></li>
+                                <li id="create_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 0}">
+                                    <a id="create_menu" v-on:click="menuselection = 0">
+                                        <span class="icon is-large"><i class="fas fa-image fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">Create AIMG</p>
+                                    </a>
+                                </li>
+                                <li id="split_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 1}">
+                                    <a id="split_menu" v-on:click="menuselection = 1">
+                                        <span class="icon is-large"><i class="far fa-images fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">Split AIMG</p>
+                                    </a>
+                                </li>
+                                <li id="modify_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 2}">
+                                    <a id="modify_menu" v-on:click="menuselection = 2">
+                                        <span class="icon is-large"><i class="fas fa-exchange-alt fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">Modify AIMG</p>    
+                                    </a>
+                                </li>
+                                <li id="buildsprite_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 3}">
+                                    <a id="buildsprite_menu" v-on:click="menuselection = 3">
+                                        <span class="icon is-large"><i class="fas fa-border-all fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">Build Spritesheet</p>    
+                                    </a>
+                                </li>
+                                <li id="slicesprite_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 4}">
+                                    <a id="slicesprite_menu" v-on:click="menuselection = 4">
+                                        <span class="icon is-large"><i class="fas fa-th fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">Slice Spritesheet</p>    
+                                    </a>
+                                </li>
+                                <li id="settings_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 5}">
+                                    <a id="settings_menu" v-on:click="menuselection = 5">
+                                        <span class="icon is-large"><i class="fas fa-cog fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">Settings</p>
+                                    </a>
+                                </li>
+                                <li id="about_box" class="menu-item" v-bind:class="{'is-selected': menuselection == 6}">
+                                    <a id="about_menu" v-on:click="menuselection = 6">
+                                        <span class="icon is-large"><i class="fas fa-info-circle fa-2x fa-inverse"></i></span>
+                                        <p class="is-white-d">About</p>
+                                    </a>
+                                </li>
                             </ul>
                         </aside>
                     </div>
@@ -68,7 +82,8 @@
                     <ModifyFragment v-show="menuselection == 2"/>
                     <BuildSpritesheetFragment v-show="menuselection == 3"/>
                     <SliceSpritesheetFragment v-show="menuselection == 4"/>
-                    <AboutFragment v-show="menuselection == 5"/>
+                    <SettingsFragment v-show="menuselection == 5"/>
+                    <AboutFragment v-show="menuselection == 6"/>
                 </td>
             </tr>
         </tbody>
@@ -111,6 +126,9 @@
           data.menuselection = 0;
         }
         console.log('menuselection', data.menuselection);
+      },
+      testlog: function () {
+          console.log("pressed!");
       }
     }
   };
--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const createWindow = () => {
     });
     mainWindow.setMenu(null);
     mainWindow.loadURL(require('url').format({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join(__dirname, './dist/index.html'),
         protocol: 'file:',
         slashes: true
     }));
