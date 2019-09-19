f20559790f28476d54764df628f23c4d04ed2fbf
StahlFerro <StahlFerro@users.noreply.github.com>
2019-09-20 01:18:51 +0700

    Removed all old javascript fragments

--- jsbrain/Client.js
+++
@@ -1,8 +0,0 @@
-const zerorpc = require('zerorpc');
-let client = new zerorpc.Client({heartbeatInterval: 300000});
-client.connect("tcp://127.0.0.1:4242");
-
-const ImageViewer = require('iv-viewer').default;
-
-module.exports.client = client;
-module.exports.ImageViewer = ImageViewer;
--- jsbrain/SliceSpritesheetFragment.js
+++
@@ -1,7 +0,0 @@
-console.log('splitfragment.js loaded!');
-const remote = require('electron').remote;
-const dialog = remote.dialog;
-const session = remote.getCurrentWebContents().session;
-const { client } = require('./Client.js');
-const { mboxClear, mboxError, mboxSuccess } = require('./MessageBox.js');
-const { escapeHtml } = require('./Utils.js')
