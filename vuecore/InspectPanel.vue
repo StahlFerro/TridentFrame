626119f5a121268ef37ef792c2bce8d2c7c8506a
StahlFerro <StahlFerro@users.noreply.github.com>
2019-09-25 14:41:45 +0700

    Added InspectPanel

---
+++ vuecore/InspectPanel.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="inspect_panel" class="container" style="padding:10px;">
+        <table class="table is-borderless" style="padding: 5px;" width="100%">
+            <tr>
+                <td class="silver-bordered force-center is-paddingless" width="45%" style="height: 380px;">
+                </td>
+                <td width="55%" class="is-paddingless silver-bordered">
+                </td>
+            </tr>
+        </table>
+    </div>
+</template>
