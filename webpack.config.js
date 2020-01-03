dcf70eb5127843ff9a90f6dffa953f4622d23142
StahlFerro <StahlFerro@users.noreply.github.com>
2020-01-03 21:08:07 +0700

    Updated version_scribe.py markdown description writing, NODE_ENV now used to control webpack.config.js's mini css plugin hmr and node-loader/native-ext-loader on dev/prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,58 +3,63 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-module.exports = {
-  entry: './app.js',
-  target: 'electron-renderer',
-  node: {
-    __dirname: false,
-    __filename: false
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader' },
-      { test: /\.vue$/, use: 'vue-loader' },
-      { test: /\.css$/, use: [{
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          name: 'style.css',
-          outputPath: 'css/',
-          // publicPath: '../',
-          hmr: process.env.NODE_ENV == 'development'
+module.exports = env => {
+  console.log("NODE ENV", env.NODE_ENV);
+  let node_loader = env.NODE_ENV === "DEV"? "node-loader" : "native-ext-loader";
+  console.log("used node_loader:", node_loader);
+  return {
+    entry: './app.js',
+    target: 'electron-renderer',
+    node: {
+      __dirname: false,
+      __filename: false
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, use: 'babel-loader' },
+        { test: /\.vue$/, use: 'vue-loader' },
+        { test: /\.css$/, use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            name: 'style.css',
+            outputPath: 'css/',
+            // publicPath: '../',
+            hmr: env.NODE_ENV == 'DEV'
+          },
+        }, 'css-loader',]},
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: 'file-loader',
         },
-      }, 'css-loader',]},
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: 'file-loader',
-      },
-      { test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: 'webfonts/',
-          // publicPath: '../',
-        }
-      }]},
-      { test: /\.node$/, loader: "native-ext-loader", }
-      // { test: /\.node$/, use: 'node-loader' },
-    ]
-  },
-  devServer: {
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-      chunkFilename: '[id].css',
-    }),
-    new VueLoaderPlugin(),
-    // new BundleAnalyzerPlugin(),
-  ],
-  output: {
-    filename: 'bundle.js',
-    // path: path.resolve(__dirname, 'release/html'),
-  },
+        { test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/, use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'webfonts/',
+            // publicPath: '../',
+          }
+        }]},
+        { test: /\.node$/, loader: node_loader, }
+        // { test: /\.node$/, use: 'node-loader' },
+      ]
+    },
+    devServer: {
+      hot: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+        chunkFilename: '[id].css',
+      }),
+      new VueLoaderPlugin(),
+      // new BundleAnalyzerPlugin(),
+    ],
+    output: {
+      filename: 'bundle.js',
+      // path: path.resolve(__dirname, 'release/html'),
+    },
+  };
 };
--- writer/version_scribe.py
+++ writer/version_scribe.py
@@ -3,30 +3,49 @@
 import json
 import os
 import platform
+import hashlib
 from pprint import pprint
 
 
 SCRIBE_PATH = os.path.realpath(__file__)
 SCRIBE_DIR = os.path.dirname(SCRIBE_PATH)
+MD_DIR = os.path.join(SCRIBE_DIR, "markdown")
 PROJECT_DIR = os.path.abspath(os.path.join(SCRIBE_DIR, ".."))
 RELEASES_DIR = os.path.join(PROJECT_DIR, "release/tridentframe/")
 PACKAGEJSON_PATH = os.path.join(PROJECT_DIR, "package.json")
 
 package_json = {}
+targets = []
+scribe_texts = []
+os_name = ""
 with open(PACKAGEJSON_PATH, "r") as jsonfile:
     package_json = json.load(jsonfile)
 
 build_info = package_json['build']
+VERSION = package_json['version']
+print("VERSION", VERSION)
 
-if platform.system() == 'Windows':
+os_name = platform.system()
+if os_name == 'Windows':
     targets = build_info['win']['target']
-elif platform.system() == 'Linux':
+elif os_name == 'Linux':
     targets = build_info['linux']['target']
 
 releases_content = (os.path.join(RELEASES_DIR, diritem) for diritem in os.listdir(RELEASES_DIR))
 release_files = [r for r in releases_content if os.path.isfile(r)]
 for target in targets:
     print(target)
-    relfile = next((r for r in release_files if target == os.path.splitext(os.path.basename(r))[1][1:]), "None")
+    relfile = next((r for r in release_files if target == os.path.splitext(os.path.basename(r))[1][1:]), None)
     print(relfile)
-    
+    sha256_hash = hashlib.sha256()
+    hashstring = ""
+    if relfile:
+        with open(relfile, "rb") as f:
+            for byte_block in iter(lambda: f.read(4096), b""):
+                sha256_hash.update(byte_block)
+            hashstring = sha256_hash
+    scribe_texts.append(f"- **{target}** {os.path.basename(relfile)}")
+    scribe_texts.append(f"  SHA256: `{hashstring.hexdigest()}`")
+
+with open(os.path.join(MD_DIR, "version.md"), "w") as mdfile:
+    mdfile.write("\n".join(scribe_texts))
