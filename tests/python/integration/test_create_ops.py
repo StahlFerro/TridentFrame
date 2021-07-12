9fcf8ba3b020231483fba41f704fdaef8f056de5
StahlFerro <StahlFerro@users.noreply.github.com>
2021-07-13 01:01:55 +0700

    Modified criteria pack 001 json fixture to have apng_is_optimized: True; test_build_apng asserts inspected metadata of created temporary image

--- tests/python/integration/test_create_ops.py
+++ tests/python/integration/test_create_ops.py
@@ -1,7 +1,9 @@
 import json
 import pytest
 from pathlib import Path
+from apng import APNG
 from pycore.models.criterion import CriteriaBundle, CreationCriteria, GIFOptimizationCriteria, APNGOptimizationCriteria
+from pycore.inspect_ops import inspect_animated_png
 from pycore.create_ops import _build_apng
 
 
@@ -21,3 +23,16 @@ def test_build_apng(scaffold_temp_dir):
     tmp_out_path = scaffold_temp_dir.joinpath("not_tetris.png")
     out_path = _build_apng(TEST_SEQUENCE_PATH, tmp_out_path, crbundle)
     assert out_path == tmp_out_path
+
+    metadata = inspect_animated_png(out_path, APNG.open(out_path))
+    assert metadata.name['value'] == "not_tetris.png"
+    assert metadata.base_filename['value'] == "not_tetris"
+    assert metadata.width['value'] == 128
+    assert metadata.height['value'] == 128
+    assert metadata.loop_count['value'] == 0
+    assert metadata.fps['value'] == 5
+    assert metadata.average_delay['value'] == 200
+    assert metadata.average_delay['value'] == 200
+    assert metadata.frame_count['value'] == 4
+    assert metadata.has_transparency['value']
+    assert metadata.hash_sha1['value'] == "cef0c9f6938a9d8d36a10e4bc13debeba5cb43e4"
