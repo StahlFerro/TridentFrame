95fa5a78ddaeca4eef9c9762bee83ef6ee7235e6
StahlFerro <StahlFerro@users.noreply.github.com>
2022-08-29 01:16:36 +0700

    Electron builder now has platform-specific configs on shipping-configs

--- writer/info_scribe.py
+++ writer/info_scribe.py
@@ -2,6 +2,7 @@ import json
 import platform
 import hashlib
 from pathlib import Path
+from typing import List
 
 
 SCRIBE_PATH = Path(__file__).resolve()
@@ -10,27 +11,34 @@ MD_DIR = SCRIBE_DIR.joinpath("markdown")
 PROJECT_DIR = SCRIBE_DIR.parents[0]
 RELEASES_DIR = PROJECT_DIR.joinpath("release/tridentframe/")
 PACKAGEJSON_FILE = PROJECT_DIR.joinpath("package.json")
+ELECTRON_BUILDER_CONF_FILE = ""
 
 package_json = {}
-targets = []
+build_config = {}
+targets: List[str] = []
 scribe_texts = []
 hash_info = []
 os_name = ""
 with open(PACKAGEJSON_FILE, "r") as jsonfile:
     package_json = json.load(jsonfile)
-build_info = package_json["build"]
 VERSION = package_json["version"]
 
 os_name = platform.system()
 if os_name == "Windows":
-    targets = build_info["win"]["target"]
+    ELECTRON_BUILDER_CONF_FILE = PROJECT_DIR.joinpath("shipping-configs/electron-builder-win.json")
+    with open(ELECTRON_BUILDER_CONF_FILE, "r") as jsonfile:
+        build_config = json.load(jsonfile)
+    targets = build_config["win"]["target"]
     scribe_texts.append(f"- Windows 7 SP1, 10 (64bit)")
 elif os_name == "Linux":
-    targets = build_info["linux"]["target"]
+    ELECTRON_BUILDER_CONF_FILE = PROJECT_DIR.joinpath("shipping-configs/electron-builder-linux.json")
+    with open(ELECTRON_BUILDER_CONF_FILE, "r") as jsonfile:
+        build_config = json.load(jsonfile)
+    targets = build_config["linux"]["target"]
     scribe_texts.append(f"- Ubuntu 18.04 LTS (64bit)")
 release_files = [RELEASES_DIR.joinpath(diritem) for diritem in RELEASES_DIR.glob("*") if Path.is_file(diritem)]
 for target in targets:
-    release_file = next((r for r in release_files if target == r.suffix[1:]), None)
+    release_file = next((r for r in release_files if target == r.suffix[1:] or target in r.name), None)
     sha256_hash = hashlib.sha256()
     hash_obj = ""
     if release_file:
