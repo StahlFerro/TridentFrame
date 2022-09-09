c8ec325c067496c499a721eec30e123cef56d963
StahlFerro <StahlFerro@users.noreply.github.com>
2022-09-10 00:53:40 +0700

    Updated npm packages

--- src/view/SettingsPanel.vue
+++ src/view/SettingsPanel.vue
@@ -155,6 +155,46 @@
 
         <div
           v-show="settings_tab_selection == 1"
+          class="settings-subpanel-window">
+          <div class="card">
+            <header class="card-header">
+              <p class="card-header-title">
+                Card title
+              </p>
+              <a href="#collapsible-card" data-action="collapse" class="card-header-icon is-hidden-fullscreen" aria-label="more options">
+                <span class="icon">
+                  <i class="fas fa-angle-down" aria-hidden="true"></i>
+                </span>
+              </a>
+            </header>
+            <div id="collapsible-card" class="is-collapsible is-active">
+              <div class="card-content">
+                <p class="title is-4">
+                  “There are two hard things in computer science: cache invalidation, naming things, and off-by-one
+                  errors.”
+                </p>
+                <p class="subtitle is-5">
+                  Jeff Atwood
+                </p>
+              </div>
+              <footer class="card-footer">
+                <p class="card-footer-item">
+                  <span>
+                    View on <a href="https://twitter.com/codinghorror/status/506010907021828096">Twitter</a>
+                  </span>
+                </p>
+                <p class="card-footer-item">
+                  <span>
+                    Share on <a href="#">Facebook</a>
+                  </span>
+                </p>
+              </footer>
+            </div>
+          </div>
+        </div>
+
+        <div
+          v-show="settings_tab_selection == 2"
           class="settings-subpanel-window"
         >
           <table class="table is-borderless" style="padding: 5px" width="100%">
@@ -211,7 +251,7 @@
         </div>
 
         <div
-          v-show="settings_tab_selection == 2"
+          v-show="settings_tab_selection == 3"
           class="settings-subpanel-about"
         >
           <div class="about-content">
